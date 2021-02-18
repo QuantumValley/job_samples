53adb7a4cf27574b6b615c6b3b2eb239a257985f
QuantumValley <55360087+QuantumValley@users.noreply.github.com>
2021-02-17 23:29:14 -0500

    Update CitiesInfo.java

--- weather_app/CitiesInfo.java
+++ weather_app/CitiesInfo.java
@@ -10,29 +10,17 @@ public class CitiesInfo {
         this.currentCondition = currentCondition;
     }
 
-    /**
-     * @requires temp is intialzed
-     * @ensures temp is returned
-     * @return
-     */
+    
     public String getTemp() {
         return temp;
     }
 
-    /**
-     * @requires name is intialzed
-     * @ensures name is returned
-     * @return
-     */
+    
     public String getName() {
         return name;
     }
 
-    /**
-     * @requires currentCondition is intialzed
-     * @ensures currentCondition is returned
-     * @return
-     */
+    
     public String getCurrentCondition() {
         return currentCondition;
     }
